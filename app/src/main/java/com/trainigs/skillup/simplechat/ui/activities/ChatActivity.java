6e7cd7b026d65c1cadeaabefa125847decf738fa
ADMIN <managadze@gmail.com>
2016-04-17 14:44:06 +0400

    Add Chat Activity

---
+++ app/src/main/java/com/trainigs/skillup/simplechat/ui/activities/ChatActivity.java
@@ -0,0 +1,15 @@
+package com.trainigs.skillup.simplechat.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.trainigs.skillup.simplechat.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
